3640e6a05b35d96e001c6dc005bf4cf032f4418c
Malinda Mosholder <m.m.mosholder@gmail.com>
2019-01-14 20:47:51 -0700

    update menu button

--- components/TextWithButton.vue
+++ components/TextWithButton.vue
@@ -8,7 +8,7 @@
         </div>
         <div class="ni-text-block-right">
           <div class="ni-text-block-button">
-            <nuxt-link class="button button-green" :to="blok.button_url">{{ blok.button_text }}</nuxt-link>
+            <a class="button button-green" href="blok.button_url">{{ blok.button_text }}</a>
           </div>
         </div>
       </div>
