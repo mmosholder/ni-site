8d4d9fbe656aa317c097cd6ea6dfcce31fb6d24a
Malinda Mosholder <m.m.mosholder@gmail.com>
2019-01-19 17:43:31 -0700

    update tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ module.exports = {
         content: "{{escape description }}"
       },
       { property: "og:title", content: "New Image Brewing" },
-      { property: "og:url", content: "//www.nibrewing.com/" },
+      { property: "og:url", content: "https://www.nibrewing.com/" },
 
       { property: "og:type", content: "website" },
       { property: "og:site_name", content: "New Image Brewing" },
@@ -61,7 +61,7 @@ module.exports = {
       },
       {
         property: "og:image",
-        content: "//www.nibrewing.com/images/ni-social-share.jpg"
+        content: "https://www.nibrewing.com/images/ni-social-share.jpg"
       },
       { property: "og:image:width", content: "1200" },
       { property: "og:image:height", content: "630" },
@@ -73,7 +73,7 @@ module.exports = {
         content:
           "Juicy, citrusy IPAs, unique takes on sours, and everything in between. Come to our taproom in Olde Town Arvada to enjoy our craft beers, craft cocktails, tapas-style food, and social dining."
       },
-      { property: "twitter:image", content: "//www.nibrewing.com/images/ni-social-share.jpg" },
+      { property: "twitter:image", content: "https://www.nibrewing.com/images/ni-social-share.jpg" },
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
